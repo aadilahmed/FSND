241af0ef1043013b62f609e38847d92c4716427d
aadil <aadilahmed0@gmail.com>
2020-08-15 19:55:57 -0700

    initial commit

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -10,4 +10,4 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgres://postgres:Star2Craft@localhost:5432/fyyur'
